9bbc83fcb415bf2afbcbbd94b0d51e8e0429e404
Anynomous <156112254+yosintv2@users.noreply.github.com>
2024-12-03 10:10:39 +0900

    Create genzai.php

---
+++ genzai.php
@@ -0,0 +1,138 @@
+[
+  {
+    "data": [
+      {
+        "id": "11",
+        "title": "Nepal Premier League",
+        "homeTeam": "Biratnagar Kings",
+        "awayTeam": "Janakpur Bolts",
+        "homeImg": "https://media.cricnepal.com/assets/Biratnagar-Kings-NPL-logo.webp",
+        "awayImg": "https://media.cricnepal.com/assets/Janakpur-Bolts-NPL-logo.webp",
+        "href": "https://cricket.genzaitv.com/nepal-premier-league-npl-2024/",
+        "time": "2024-04-29T15:05:00+09:00",
+        "type": "cricketz"
+      },
+      {
+        "id": "12",
+        "title": "Nepal Premier League",
+        "homeTeam": "Kathmandu Gurkhas",
+        "awayTeam": "Chitwan Rhinos",
+        "homeImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/392000/392043.png",
+        "awayImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/392000/392044.jpg",
+        "href": "https://cricket.genzaitv.com/nepal-premier-league-npl-2024/",
+        "time": "2024-04-30T02:10:00+09:00",
+        "type": "cricketz"
+      },
+      {
+        "id": "13",
+        "title": "Nepal Premier League",
+        "homeTeam": "Janakpur Bolts",
+        "awayTeam": "Karnali Yaks",
+        "homeImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/392000/392045.png",
+        "awayImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/392000/392046.png",
+        "href": "https://cricket.genzaitv.com/nepal-premier-league-npl-2024/",
+        "time": "2024-04-30T05:30:00+09:00",
+        "type": "cricketz"
+      },
+      {
+        "id": "14",
+        "title": "Nepal Premier League",
+        "homeTeam": "SudurPaschim Royals",
+        "awayTeam": "Biratnagar Kings",
+        "homeImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/391900/391989.png",
+        "awayImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/391900/391936.jpg",
+        "href": "https://cricket.genzaitv.com/nepal-premier-league-npl-2024/",
+        "time": "2024-04-30T08:30:00+09:00",
+        "type": "cricketz"
+      },
+      {
+        "id": "15",
+        "title": "Nepal Premier League",
+        "homeTeam": "Pokhara Avengers",
+        "awayTeam": "Chitwan Rhinos",
+        "homeImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/391900/391990.png",
+        "awayImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/392000/392044.jpg",
+        "href": "https://cricket.genzaitv.com/nepal-premier-league-npl-2024/",
+        "time": "2024-04-30T09:30:00+09:00",
+        "type": "cricketz"
+      },
+      {
+        "id": "16",
+        "title": "Nepal Premier League",
+        "homeTeam": "Kathmandu Gurkhas",
+        "awayTeam": "Karnali Yaks",
+        "homeImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/392000/392043.png",
+        "awayImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/392000/392046.png",
+        "href": "https://cricket.genzaitv.com/nepal-premier-league-npl-2024/",
+        "time": "2024-04-30T12:30:00+09:00",
+        "type": "cricketz"
+      },
+      {
+        "id": "17",
+        "title": "Nepal Premier League",
+        "homeTeam": "Lumbini Lions",
+        "awayTeam": "Biratnagar Kings",
+        "homeImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/391900/391942.jpg",
+        "awayImg": "https://img1.hscicdn.com/image/upload/f_auto,t_ds_square_w_160,q_50/lsci/db/PICTURES/CMS/391900/391936.jpg",
+        "href": "https://cricket.genzaitv.com/nepal-premier-league-npl-2024/",
+        "time": "2024-04-30T13:30:00+09:00",
+        "type": "cricketz"
+      },
+      {
+        "id": "12196745",
+        "title": "Border-Gavaskar Trophy",
+        "homeTeam": "Australia",
+        "awayTeam": "India",
+        "homeImg": "https://api.sofascore.app/api/v1/team/187746/image",
+        "awayImg": "https://api.sofascore.app/api/v1/team/187765/image",
+        "href": "https://cricket.genzaitv.com/",
+        "time": "2024-04-30T15:00:00+09:00",
+        "type": "cricket"
+      },
+      {
+        "id": "12303205",
+        "title": "ICC World Test Championship",
+        "homeTeam": "South Africa",
+        "awayTeam": "Sri Lanka",
+        "homeImg": "https://api.sofascore.app/api/v1/team/187747/image",
+        "awayImg": "https://api.sofascore.app/api/v1/team/187756/image",
+        "href": "https://tinyurl.com/genzaicricket",
+        "time": "2024-04-30T18:00:00+09:00",
+        "type": "cricket"
+      },
+      {
+        "id": "12325835",
+        "title": "One Day International",
+        "homeTeam": "West Indies",
+        "awayTeam": "Bangladesh",
+        "homeImg": "https://api.sofascore.app/api/v1/team/187760/image",
+        "awayImg": "https://api.sofascore.app/api/v1/team/187744/image",
+        "href": "https://cricket.genzaitv.com/",
+        "time": "2024-05-01T02:10:00+09:00",
+        "type": "cricket"
+      },
+      {
+        "id": "12506060",
+        "title": "T20 International",
+        "homeTeam": "Zimbabwe",
+        "awayTeam": "Pakistan",
+        "homeImg": "https://api.sofascore.app/api/v1/team/187745/image",
+        "awayImg": "https://api.sofascore.app/api/v1/team/187754/image",
+        "href": "https://cricket.genzaitv.com/",
+        "time": "2024-04-30T10:30:00+09:00",
+        "type": "cricket"
+      },
+      {
+        "id": "12506061",
+        "title": "T20 International",
+        "homeTeam": "Zimbabwe",
+        "awayTeam": "Pakistan",
+        "homeImg": "https://api.sofascore.app/api/v1/team/187745/image",
+        "awayImg": "https://api.sofascore.app/api/v1/team/187754/image",
+        "href": "https://cricket.genzaitv.com/",
+        "time": "2024-04-30T15:10:00+09:00",
+        "type": "cricket"
+      }
+    ]
+  }
+]
